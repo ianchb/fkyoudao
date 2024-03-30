08e7ce495e62b41c120db9c886ffa133ed84a649
Cheng Haobo <i@ap.icu>
2024-03-31 03:11:36 +0800

    Enable ADB and MTP after booting

--- scripts/scripts/start.sh
+++ scripts/scripts/start.sh
@@ -5,6 +5,9 @@ MODDIR=${0%/*}
 # run android settings when Android boot
 start_wake () {
   am start -n com.android.settings/com.android.settings.Settings
+  settings put global development_settings_enabled 1
+  settings put global adb_enabled 1
+  svc usb setFunctions mtp
 }
 
 start_wake
